3a19d144bedb6fd5d4294247b6da4d856d690764
tsiangleo <tsiangleo@gmail.com>
2017-04-22 23:42:34 +0800

    add tensorflow

--- app/src/main/java/com/github/tsiangleo/tfdemo/MainActivity.java
+++ app/src/main/java/com/github/tsiangleo/tfdemo/MainActivity.java
@@ -17,6 +17,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 
 /**
  * Created by tsiang on 2016/11/26.
@@ -28,6 +30,7 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
     private Button imgChooseButton,startRecognitionButton;
     private ImageView imageView;
     private TextView statusTextView;
+    private Bitmap bitmap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,19 +60,11 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
     private void chooseFile() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/*");
-        /**
-         *
-         //intent.setType(“image/*”);//选择图片
-         //intent.setType(“audio/*”); //选择音频
-         //intent.setType(“video/*”); //选择视频 （mp4 3gp 是android支持的视频格式）
-         //intent.setType(“video/*;image/*”);//同时选择视频和图片
-         */
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         try {
-            startActivityForResult( Intent.createChooser(intent, "Select a File to Upload"), 1);
+            startActivityForResult( Intent.createChooser(intent, "选择文件"), 1);
         } catch (android.content.ActivityNotFoundException ex) {
-            // Potentially direct the user to the Market with a Dialog
-            Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "请先安装一个文件管理器.", Toast.LENGTH_SHORT).show();
         }
 
     }
@@ -91,7 +86,7 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
                         statusTextView.setText("文件必须是jpg/png/bmp格式的，请重新选择！");
                         imgChooseButton.setText("重新选择文件");
                     }else{
-                        Bitmap bitmap = BitmapFactory.decodeFile(realPath,null);
+                        bitmap = BitmapFactory.decodeFile(realPath,null);
                         imageView.setVisibility(View.VISIBLE);
                         imageView.setImageBitmap(bitmap);
                         statusTextView.setText("当前选择文件:"+realPath);
@@ -100,7 +95,7 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
                     }
 
                 }catch (java.lang.SecurityException e){
-                    statusTextView.setText("没有授权读取SD卡，请先授权:"+e.getMessage());
+                    statusTextView.setText("应用需要读取SD卡，请先授权读取SD卡！");
                 }catch (Exception e){
                     Log.w(TAG,"onActivityResult:"+e);
                     statusTextView.setText("选择文件出错:"+e.getMessage());
@@ -112,16 +107,19 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
 
     private void recognition() {
         try {
-//            RecognitionService recognitionService = new RecognitionService(MainActivity.this);
-//            recognitionService.init();
-//            recognitionService.recognize();
-            statusTextView.setText("识别结果：7");
+            RecognitionService recognitionService = new RecognitionService(MainActivity.this);
+
+            float[] outSoftmax = recognitionService.recognize(bitmap);
+
+            statusTextView.setText("识别结果："+recognitionService.argmax(outSoftmax));
+
+            Toast.makeText(this,"outSoftmax:"+ Arrays.toString(outSoftmax),Toast.LENGTH_LONG).show();
 
             startRecognitionButton.setVisibility(View.GONE);
             imgChooseButton.setText("继续选择文件");
             imgChooseButton.setVisibility(View.VISIBLE);
         } catch (Exception e) {
-            Log.e(TAG, "RecognitionTask.doInBackground:" + e);
+            Log.e(TAG, "识别中发生异常:" + e);
 
             statusTextView.setText("识别中发生异常！"+e.getMessage());
 
@@ -132,56 +130,17 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
     }
 
     /**
-     * 从SD卡上读取需要用户授权。
-     * http://blog.csdn.net/likesidehu/article/details/52668879
-     *
+     * 读取SD卡需要用户授权。
      */
     @SuppressLint("NewApi")
     private void requestReadExternalPermission() {
-        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             Log.d(TAG, "READ permission IS NOT granted...");
 
-            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
 
-            } else {
-                // 0 是自己定义的请求coude
-                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
-            }
         } else {
             Log.d(TAG, "READ permission is granted...");
-        }
-    }
-
-    /**
-     * @param requestCode
-     * @param permissions
-     * @param grantResults
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        Log.d(TAG, "requestCode=" + requestCode + "; --->" + permissions.toString()
-                + "; grantResult=" + grantResults.toString());
-        switch (requestCode) {
-            case 0: {
-
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted
-                    // request successfully, handle you transactions
-
-                } else {
-
-                    // permission denied
-                    // request failed
-                }
-
-                return;
-            }
-            default:
-                break;
-
         }
     }
 }
--- app/src/main/java/com/github/tsiangleo/tfdemo/RecognitionService.java
+++ app/src/main/java/com/github/tsiangleo/tfdemo/RecognitionService.java
@@ -1,20 +1,13 @@
 package com.github.tsiangleo.tfdemo;
 
 import android.content.Context;
-import android.icu.text.RelativeDateTimeFormatter;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.util.Log;
 
-//import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
-import static android.content.ContentValues.TAG;
 
 
 /**
@@ -23,155 +16,108 @@ import static android.content.ContentValues.TAG;
 
 
 public class RecognitionService {
-//
-//    public static final String TAG = "RecognitionService";
-//
-//    public static final String LABEL_FILE = "speaker_recognition_label_strings.txt";
-//    public static final String MODEL_FILE = "file:///android_asset/sid_one.pb"; //模型存放路径
-//
-//    public static final String INPUT_NODE = "input";       //模型中输入变量的名称
-//    public static final String OUTPUT_NODE = "out_softmax";  //模型中输出变量的名称
-//
-//    public static final int NUM_CLASSES = 5;               //样本集的类别数量
-//    public static final int BATCH_SIZE = 16;
-//    public static final int HEIGHT = 128;       //输入图片的像素高
-//    public static final int WIDTH = 100;        //输入图片的像素宽
-//    public static final int TIME_STEP = 1;
-//
-////    private double[] inputs = new double[HEIGHT*WIDTH];    //用于存储的模型输入数据
-//
-//
-//    private TensorFlowInferenceInterface inferenceInterface = new TensorFlowInferenceInterface();   //接口定义
-//
-//    public static final String[] NAMES = new String[NUM_CLASSES];    //标签对应的名字
-//
-//
-//    private Context context;
-//
-//    public RecognitionService(Context context){
-//
-//        System.loadLibrary("tensorflow_inference");
-//
-//        this.context = context;
-//
-//    }
-//
-//    /**
-//     * 从LABEL_FILE文件中读取名字
-//     * @throws Exception
-//     */
-//    private void loadNames() throws Exception{
-//        InputStream in = context.getAssets().open(LABEL_FILE); //new FileInputStream(LABEL_FILE);
-//        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-//        String line;
-//        int index = 0;
-//        while ((line = br.readLine()) != null){
-//            NAMES[index++] = line;
-//        }
-//    }
-//
-//
-//    public void init() throws Exception{
-//        int retCode = inferenceInterface.initializeTensorFlow(context.getAssets(), MODEL_FILE);  //接口初始化
-//        if (retCode != 0){
-//            throw new RuntimeException("inferenceInterface.initializeTensorFlow运行失败:retCode is "+retCode);
-//        }
-//        loadNames();
-//    }
-//
-//    /**
-//     * 输入一张语谱图的像素点矩阵（行优先），每个点已经正规化了。
-//     * @param normalizedPixelArray
-//     * @return　该语谱图属于各个标签的概率。
-//     */
-//    public float[] recognize_batch(float[] normalizedPixelArray){
-//        if(normalizedPixelArray.length != HEIGHT*WIDTH){
-//            throw new IllegalArgumentException("输入的语谱图像素矩阵的大小不对，传入的大小为:"+normalizedPixelArray.length +",需要的大小为："+(HEIGHT*WIDTH));
-//        }
-//
-//        //填充为０
-//        float[] inputs = new float[BATCH_SIZE*normalizedPixelArray.length];
-//        for(int i=0;i<normalizedPixelArray.length;i++){
-//            inputs[i] = normalizedPixelArray[i];
-//        }
-//
-//        inferenceInterface.fillNodeFloat(INPUT_NODE, new int[]{BATCH_SIZE, TIME_STEP, HEIGHT*WIDTH}, inputs);  //送入输入数据
-//        inferenceInterface.fillNodeFloat("keep_prob_placeholder",new int[]{1},new float[]{1.0f});
-//
-//
-//        int retCode = inferenceInterface.runInference(new String[]{OUTPUT_NODE});     //进行模型的推理
-//
-//        if (retCode != 0){
-//            throw new RuntimeException("inferenceInterface.runInference运行失败:retCode is "+retCode);
-//        }
-//
-//        float[] outputs = new float[NUM_CLASSES*BATCH_SIZE];    //用于存储模型的输出数据
-//        inferenceInterface.readNodeFloat(OUTPUT_NODE, outputs); //获取输出数据
-//
-//
-//        //获取第一个
-//        float[] output = new float[NUM_CLASSES];
-//        float sum = 0.0f;
-//        for(int i=0;i<NUM_CLASSES;i++){
-//            output[i] = outputs[i];
-//            sum += output[i];
-//        }
-//        Log.i(TAG, "probability sum of one spectrogram is:"+sum+",are:"+ Arrays.toString(output));
-//        return output;
-//    }
-//
-//
-//    /**
-//     * 输入一张语谱图的像素点矩阵（行优先），每个点已经正规化了。
-//     * @param normalizedPixelArray
-//     * @return　该语谱图属于各个标签的概率。
-//     */
-//    public float[] recognize(float[] normalizedPixelArray){
-//        if(normalizedPixelArray.length != HEIGHT*WIDTH){
-//            throw new IllegalArgumentException("输入的语谱图像素矩阵的大小不对，传入的大小为:"+normalizedPixelArray.length +",需要的大小为："+(HEIGHT*WIDTH));
-//        }
-//
-//        inferenceInterface.fillNodeFloat(INPUT_NODE, new int[]{1, TIME_STEP, HEIGHT*WIDTH}, normalizedPixelArray);  //送入输入数据
-//        inferenceInterface.fillNodeFloat("keep_prob_placeholder",new int[]{1},new float[]{1.0f});
-//
-//
-//        int retCode = inferenceInterface.runInference(new String[]{OUTPUT_NODE});     //进行模型的推理
-//
-//        if (retCode != 0){
-//            throw new RuntimeException("inferenceInterface.runInference运行失败:retCode is "+retCode);
-//        }
-//
-//        float[] outputs = new float[NUM_CLASSES];    //用于存储模型的输出数据
-//        inferenceInterface.readNodeFloat(OUTPUT_NODE, outputs); //获取输出数据
-//
-//
-//        float sum = 0.0f;
-//        for(int i=0;i<NUM_CLASSES;i++){
-//            sum += outputs[i];
-//        }
-////        Log.i(TAG, "probability sum of one spectrogram is:"+sum+",are:"+ Arrays.toString(outputs));
-//
-//        return outputs;
-//    }
-//
-//
-//    public static int argmax(float[] prob){
-//        int result = 0;
-//        for(int i=1;i<prob.length;i++) {
-//            if (prob[result] < prob[i]) {
-//                result = i;
-//            }
-//        }
-//        return result;
-//    }
-//
-//    public static int argmax(int[] prob){
-//        int result = 0;
-//        for(int i=1;i<prob.length;i++) {
-//            if (prob[result] < prob[i]) {
-//                result = i;
-//            }
-//        }
-//        return result;
-//    }
+
+    public static final String TAG = "RecognitionService";
+
+    public static final String MODEL_FILE = "file:///android_asset/mnist_output.pb"; //模型存放路径
+
+    public static final String INPUT_NODE = "input";       //模型中输入变量的名称
+    public static final String OUTPUT_NODE = "out_softmax";  //模型中输出变量的名称
+
+    public static final int NUM_CLASSES = 10;               //样本集的类别数量
+
+    public static final int HEIGHT = 28;       //输入图片的像素高
+    public static final int WIDTH = 28;        //输入图片的像素宽
+
+    private TensorFlowInferenceInterface inferenceInterface = new TensorFlowInferenceInterface();   //接口定义
+
+
+    private Context context;
+
+    public RecognitionService(Context context){
+        System.loadLibrary("tensorflow_inference");
+        this.context = context;
+
+        int retCode = inferenceInterface.initializeTensorFlow(context.getAssets(), MODEL_FILE);  //接口初始化
+
+        if (retCode != 0){
+            throw new RuntimeException("inferenceInterface.initializeTensorFlow运行失败:retCode is "+retCode);
+        }
+    }
+
+    /**
+     * 输入一张语谱图的像素点矩阵（行优先），每个点已经正规化了。
+     * @param bitmap
+     * @return　该语谱图属于各个标签的概率。
+     */
+    public float[] recognize(Bitmap bitmap){
+        float[] normalizedPixelArray = bitmap2FloatArray(bitmap);
+
+        Log.i(TAG,"normalizedPixelArray:"+Arrays.toString(normalizedPixelArray));
+
+        if(normalizedPixelArray.length != HEIGHT*WIDTH){
+            throw new IllegalArgumentException("输入图片的像素矩阵的大小不对，传入的大小为:"+normalizedPixelArray.length +",需要的大小为："+(HEIGHT*WIDTH));
+        }
+
+        inferenceInterface.fillNodeFloat(INPUT_NODE, new int[]{1, HEIGHT, WIDTH}, normalizedPixelArray);  //送入输入数据
+        inferenceInterface.fillNodeFloat("keep_prob_placeholder",new int[]{1},new float[]{1.0f});
+
+
+        int retCode = inferenceInterface.runInference(new String[]{OUTPUT_NODE});     //进行模型的推理
+
+        if (retCode != 0){
+            throw new RuntimeException("inferenceInterface.runInference运行失败:retCode is "+retCode);
+        }
+
+        float[] outputs = new float[NUM_CLASSES];    //用于存储模型的输出数据
+        inferenceInterface.readNodeFloat(OUTPUT_NODE, outputs); //获取输出数据
+
+        return outputs;
+    }
+
+
+    public static int argmax(float[] prob){
+        int result = 0;
+        for(int i=1;i<prob.length;i++) {
+            if (prob[result] < prob[i]) {
+                result = i;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 将bitmap转为（按行优先）一个float数组。其中的每个像素点都正则化到0到1之间。
+     * @param bitmap 灰度图，r,g,b分量都相等。
+     * @return
+     */
+    public static float[] bitmap2FloatArray(Bitmap bitmap){
+        int height = bitmap.getHeight();
+        int width = bitmap.getWidth();
+        float[] result = new float[height*width];
+        Log.i(TAG,"bitmap width:"+width+",height:"+height);
+        Log.i(TAG,"bitmap.getConfig():"+bitmap.getConfig());
+
+        int k = 0;
+
+        //行优先
+        for(int j = 0;j < height;j++){
+            for (int i = 0;i < width;i++){
+                int argb = bitmap.getPixel(i,j);
+
+                int r = Color.red(argb);
+                int g = Color.green(argb);
+                int b = Color.blue(argb);
+                int a = Color.alpha(argb);
+
+                //由于是灰度图，所以r,g,b分量是相等的。
+                assert(r==g && g==b);
+
+//                Log.i(TAG,i+","+j+" : argb = "+argb+", a="+a+", r="+r+", g="+g+", b="+b);
+                result[k++] = r / 255.0f;
+            }
+        }
+
+        return result;
+    }
 }
